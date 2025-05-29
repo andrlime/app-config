8085ae8d96448c4502624a1193f3bbf5d4e1fa02
Andrew Li <andrewli@u.northwestern.edu>
2025-05-29 15:08:51 -0500

    add new git branch aliases

--- shell/export/export.sh
+++ shell/export/export.sh
@@ -1,6 +1,7 @@
 source "${0:A:h}/bun.config"
 source "${0:A:h}/cpp.config"
 source "${0:A:h}/path.config"
+source "${0:A:h}/gpg.config"
 
 export ZSH="$HOME/.oh-my-zsh"
 export TERM="xterm" # One line fix to some bugs from using non-standard terminal
